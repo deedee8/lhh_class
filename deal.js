548ee255891548c3856bd6f56b143efc5187b9ec
Rachel Mohr <rrmohr@gmail.com>
2018-10-24 09:28:33 -0700

    Finish implementing deal().

--- deal.js
+++ deal.js
@@ -1,42 +1,49 @@
 const columnCount = 7;
 
 function deal() {
+    let k = 0
+    for (let j = 0; j < 7; j++) {
 
-    for (let i = 0; i < 7; i++) {
-        let card = this.cards[i];
-        card.reversed = false;
-        this.columns[i].cards.push(card);
+        for (let i = j; i < 7; i++) {
 
+            let card = this.cards[k];
+            k++
 
-        if (i == 0) card.reversed = false;
-        else card.reversed = true;
+            this.columns[i].cards.push(card);
 
-    }
-    for (let i = 1; i < 7; i++) {
-        let card = this.cards[i];
-        card.reversed = false;
-        this.columns[i].cards.push(card);
-
-        if (i == 1) card.reversed = false;
-        else card.reversed = true;
-
-    }
-    for (let i = 2; i < 7; i++) {
-        let card = this.cards[i];
-        card.reversed = false;
-        this.columns[i].cards.push(card);
-
-        if (i == 2) card.reversed = false;
-        else card.reversed = true;
-
-    }
-    for (let i = 3; i < 7; i++) {
-        let card = this.cards[i];
-        card.reversed = false;
-        this.columns[i].cards.push(card);
-
-        if (i == 3) card.reversed = false;
-        else card.reversed = true;
+            if (i == j) {
+                card.reversed = false;
+            }
+            else card.reversed = true;
+        }
 
     }
 }
+    /*
+        for (let i = 1; i < 7; i++) {
+            let card = this.cards[i];
+            card.reversed = false;
+            this.columns[i].cards.push(card);
+    
+            if (i == 1) card.reversed = false;
+            else card.reversed = true;
+    
+        }
+        for (let i = 2; i < 7; i++) {
+            let card = this.cards[i];
+            card.reversed = false;
+            this.columns[i].cards.push(card);
+    
+            if (i == 2) card.reversed = false;
+            else card.reversed = true;
+    
+        }
+        for (let i = 3; i < 7; i++) {
+            let card = this.cards[i];
+            card.reversed = false;
+            this.columns[i].cards.push(card);
+    
+            if (i == 3) card.reversed = false;
+            else card.reversed = true;
+    
+        }*/
