329ec5e7441a2ae84588533ab5502a07ed3afcd0
Rachel Mohr <rrmohr@gmail.com>
2019-01-09 01:05:42 -0800

    Implement card-moving rules.

--- cardClicked.js
+++ cardClicked.js
@@ -1,14 +1,35 @@
 function cardClicked(card, column, lastCard, lastColumn) {
-if(lastCard == null){
+  if (card.reversed == true) {
+  }
 
-card.selected = true;
-this.remember (card,column);
-}
-else {
-   alert (lastCard.name());
-   lastCard.selected = false;
-   this.forget();
-}
+  if (lastCard == null) {
+
+    card.selected = true;
+    this.remember(card, column);
+  }
+  else {
+
+    if(lastCard == card) {
+      this.forget();
+      card.selected = false;
+      return;
+    }
+
+    if (lastCard.color == card.color) {
+      return;
+    }
+  
+    if (lastCard.value != card.value - 1) {
+      return;
+    }
+
+    let cardsToMove = lastColumn.popFrom(lastCard);
+
+    column.append(cardsToMove)
+
+    lastCard.selected = false;
+    this.forget();
+  }
 }
 /*
   //card.reversed = ! card.reversed;
